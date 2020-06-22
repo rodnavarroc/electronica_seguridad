21adb9543e62b33153b6a4dcf6d0ec6add45fd5a
rodnavarroc <rodolfo.0926@hotmail.com>
2020-06-21 19:48:44 -0500

    Commit inicial

---
+++ index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Electrónica | Cámaras de Seguridad</title>
+	<meta charset="utf-8">
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Patua+One&display=swap" rel="stylesheet"> 
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<script src="https://kit.fontawesome.com/94f47f52fa.js" crossorigin="anonymous"></script>
+</head>
+<body style="font-family: Open Sans; background-image: url(https://i.ya-webdesign.com/images/website-background-patterns-png-2.png); background-repeat:no-repeat; -webkit-background-size:cover; -moz-background-size:cover; -o-background-size:cover; background-size:cover; background-position:center;">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: #0c996a;">
+    <div class="container">
+      <a class="navbar-brand" href="#">Logo</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#"><i class="fas fa-sign-in-alt"></i>&nbsp;Acceder</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#"><i class="fas fa-shopping-cart"></i>&nbsp;Carrito</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#"><i class="fas fa-shopping-bag"></i>&nbsp;Productos</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#"><i class="fas fa-cog"></i>&nbsp;Servicios</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#"><i class="fas fa-id-badge"></i>&nbsp;Contacto</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Page Content -->
+  <div class="container">
+	<br><br><br>
+    <div class="row">
+      <!-- /.col-lg-3 -->
+
+      <div class="col-lg-12">
+          <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+		    <ol class="carousel-indicators">
+		      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+		      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+		      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+		    </ol>
+		    <div class="carousel-inner" role="listbox">
+		      <!-- Slide One - Set the background image for this slide in the line below -->
+		      <div class="carousel-item active" style="background-image: url('https://icdn3.digitaltrends.com/image/digitaltrends/netgear-ultra-smart-cam-feat.jpg')">
+		      </div>
+		      <!-- Slide Two - Set the background image for this slide in the line below -->
+		      <div class="carousel-item" style="background-image: url('https://images.idgesg.net/images/article/2017/08/geeni-vision-1-100733197-large.jpg')">
+		      </div>
+		      <!-- Slide Three - Set the background image for this slide in the line below -->
+		      <div class="carousel-item" style="background-image: url('https://blog.bestbuy.ca/wp-content/uploads/2019/04/Arlo-Ultra-4K-Wireless-Security-Camera-Review.jpg')">
+		      </div>  
+		    </div>
+		    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+		          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		          <span class="sr-only">Previous</span>
+		        </a>
+		    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+		          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+		          <span class="sr-only">Next</span>
+		        </a>
+		  </div>
+
+        <!-- /.card -->
+		
+		<br><br><center><h3 class="text-light" style="font-family: Patua One; background-color: #0c996a; padding: 1% 3%;">Marcas que manejamos</h3></center>
+          <br><br>
+          <div class="row">
+          	<div class="col-6 col-md-3"><img src="https://shieldsecurity.com.mx/img/cms/Epcom-logo.png" class="img-fluid" style="width: 100%;"></div>
+          	<div class="col-6 col-md-3"><img src="https://www.hikvision.com/UploadFile/Image/2014528155544367.png" class="img-fluid" style="width: 100%;"></div>
+          	<div class="col-6 col-md-3"><img src="https://eustatics.ezvizlife.com/ovs_mall/web/img/index/EZVIZ_logo.png?7073270671" class="img-fluid" style="width: 100%;"></div>
+          	<div class="col-6 col-md-3"><img src="https://infocomputo.co/wp-content/uploads/2019/06/hilook.png" class="img-fluid" style="width: 100%;"></div>
+          </div>
+          <br><br>
+        <!-- /.card -->
+
+        <center><h3 class="text-light" style="font-family: Patua One; background-color: #0c996a; padding: 1% 3%;">Colaboraciones</h3></center>
+          <br>
+          <div class="row">
+          	<p style="font-size: 18px;">Faltan imágenes para esta sección</p>
+          </div>
+          <br><br>
+
+      </div>
+      <!-- /.col-lg-9 -->
+
+    </div>
+
+  </div>
+  <!-- /.container -->
+  <div>
+  	<div class="container bg-dark text-light" style="padding: 1% 3%;"><br>
+      <div class="row">
+	      	<div class="col-12 col-md-3">
+		      	<center><p style="font-family: Patua One; font-size: 21px;">Productos</p></center>
+		      		<center>
+		      			<p>Cámaras de seguridad</p>
+		      			<p>Sistemas Inteligentes</p>
+		      		</center>
+	      </div>
+	      <div class="col-12 col-md-3">
+		      	<center><p style="font-family: Patua One; font-size: 21px;">Instalaciones</p></center>
+		      		<center>
+		      			<p>Paneles Solares</p>
+		      			<p>Control de Acceso</p>
+		      			<p>Sistemas de Seguridad</p>
+		      		</center>
+	      </div>
+	      <div class="col-12 col-md-3">
+		      	<center><p style="font-family: Patua One; font-size: 21px;">Contacto</p></center>
+		      		<center>
+		      			<p>¿Quiénes somos?</p>
+		      			<p>Solicita Cotización</p>
+		      		</center>
+	      </div>
+	      <div class="col-12 col-md-3">
+		      	<center><p style="font-family: Patua One; font-size: 21px;">Soporte</p></center>
+		      		<center>
+		      			<p>Preguntas Frecuentes</p>
+		      		</center>
+	      <br></div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Bootstrap core JavaScript -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+</body>
+<style type="text/css">
+	.carousel-item {
+  height: 100%;
+  min-height: 350px;
+  background: no-repeat center center scroll;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+</style>
+</html>
